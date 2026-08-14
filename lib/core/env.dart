3fc8a5ea87da74af9e636cee65ae960519116b5e
Tannake <iban_gzcy@hotmail.com>
2026-08-13 21:09:27 -0600

    update-test

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -1,6 +1,6 @@
 class Env {
   static const apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://localhost:3000/api/admin/',
+    defaultValue: 'https://ws.test.ds-clickeat.com.mx/api/admin/',
   );
 }
